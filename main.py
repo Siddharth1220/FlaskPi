8efaa0bf495f3af9355aa97bff632515f36140ed
Siddharth Vaniya <73275917+Tony-Vaniya@users.noreply.github.com>
2021-05-21 21:09:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import Rpi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14,GPIO.OUT, initial=GPIO.LOW)
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route('/')
+def home():
+    return render_template('on.html')
+
+@app.route('/on')
+def on():
+    print ('ON')
+    GPIO.output(14,GPIO.HIGH)
+    return render_template('off.html')
+
+
+@app.route('/off')
+def off():
+    print ('OFF')
+    GPIO.output(14,GPIO.HIGH)
+    return render_template('on.html')
+    
+
+
+app.run(debug=True,host='0.0.0.0')
