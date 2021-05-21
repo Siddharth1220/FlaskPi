b68b86ac538e3a2ec2a74fa46d3e46fb0bf23de1
Siddharth Vaniya <73275917+Tony-Vaniya@users.noreply.github.com>
2021-05-21 21:27:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14,GPIO.OUT, initial=GPIO.LOW)
